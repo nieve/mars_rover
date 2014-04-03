7584643ddd23057f0ffec63f12c3a7aa2f054f21
Nieve <nievegoor@gmail.com>
2014-04-03 21:57:08 +0100

    added spec for controlling rover by simple string of letters commands

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -7,19 +7,19 @@ describe "MarsRover" do
         @rover = MarsRover.new 5,5,:n,10,10
     	end
       it "should be able to move forward" do
-        @rover.move_forward
+        @rover.send('F')
         expect(@rover.position).to eq({:x => 5, :y => 6})
       end
       it "should be able to move backwards" do
-        @rover.move_backwards
+        @rover.send('B')
         expect(@rover.position).to eq({:x => 5, :y => 4})      
       end
       it "should stay facing north after moving forward" do
-        @rover.move_forward
+        @rover.send('F')
         expect(@rover.facing).to eq(:n)
       end
       it "should stay facing north after moving backwards" do
-        @rover.move_backwards
+        @rover.send('B')
         expect(@rover.facing).to eq(:n)      
       end
     end
@@ -28,19 +28,19 @@ describe "MarsRover" do
         @rover = MarsRover.new 5,5,:s,10,10
       end
       it "should be able to move forward" do
-        @rover.move_forward
+        @rover.send('F')
         expect(@rover.position).to eq({:x => 5, :y => 4})
       end
       it "should be able to move backwards" do
-        @rover.move_backwards
+        @rover.send('B')
         expect(@rover.position).to eq({:x => 5, :y => 6})
       end
       it "should stay facing south after moving forward" do
-        @rover.move_forward
+        @rover.send('F')
         expect(@rover.facing).to eq(:s)
       end
       it "should stay facing south after moving backwards" do
-        @rover.move_backwards
+        @rover.send('B')
         expect(@rover.facing).to eq(:s)      
       end
     end
@@ -49,19 +49,19 @@ describe "MarsRover" do
         @rover = MarsRover.new 5,5,:e,10,10
       end
       it "should be able to move forward" do
-        @rover.move_forward
+        @rover.send('F')
         expect(@rover.position).to eq({:x => 6, :y => 5})
       end
       it "should be able to move backwards" do
-        @rover.move_backwards
+        @rover.send('B')
         expect(@rover.position).to eq({:x => 4, :y => 5})
       end
       it "should stay facing east after moving forward" do
-        @rover.move_forward
+        @rover.send('F')
         expect(@rover.facing).to eq(:e)
       end
       it "should stay facing east after moving backwards" do
-        @rover.move_backwards
+        @rover.send('B')
         expect(@rover.facing).to eq(:e)      
       end
     end
@@ -70,19 +70,19 @@ describe "MarsRover" do
         @rover = MarsRover.new 5,5,:w,10,10
       end
       it "should be able to move forward" do
-        @rover.move_forward
+        @rover.send('F')
         expect(@rover.position).to eq({:x => 4, :y => 5})
       end
       it "should be able to move backwards" do
-        @rover.move_backwards
+        @rover.send('B')
         expect(@rover.position).to eq({:x => 6, :y => 5})
       end
       it "should stay facing west after moving forward" do
-        @rover.move_forward
+        @rover.send('F')
         expect(@rover.facing).to eq(:w)
       end
       it "should stay facing west after moving backwards" do
-        @rover.move_backwards
+        @rover.send('B')
         expect(@rover.facing).to eq(:w)      
       end
     end
