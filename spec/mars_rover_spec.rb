5b2047b14d32e9300395f89c3b560854e302e0b4
Nieve <nievegoor@gmail.com>
2014-04-03 20:47:30 +0100

    added move forward specs for all 4 directions

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,10 +2,25 @@ require "spec_helper"
 
 describe "MarsRover" do
   describe "moving" do
-    rover = MarsRover.new 5,5,:n,10,10
-    it "should be able to move forward" do
+    it "should be able to move forward north" do
+      rover = MarsRover.new 5,5,:n,10,10
       rover.move_forward
       expect(rover.position).to eq({:x => 5, :y => 6})
+    end
+    it "should be able to move forward south" do
+      rover = MarsRover.new 5,5,:s,10,10
+      rover.move_forward
+      expect(rover.position).to eq({:x => 5, :y => 4})
+    end
+    it "should be able to move forward east" do
+      rover = MarsRover.new 5,5,:e,10,10
+      rover.move_forward
+      expect(rover.position).to eq({:x => 6, :y => 5})
+    end
+    it "should be able to move forward west" do
+      rover = MarsRover.new 5,5,:w,10,10
+      rover.move_forward
+      expect(rover.position).to eq({:x => 4, :y => 5})
     end
   end
 end
