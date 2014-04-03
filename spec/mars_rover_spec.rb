51d2a0b1dfce287fa99cfa9ff63089f36070ee3d
Nieve <nievegoor@gmail.com>
2014-04-03 21:01:09 +0100

    added moving backwards when facing north spec

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,26 +1,38 @@
 require "spec_helper"
 
 describe "MarsRover" do
-  describe "moving" do
-    it "should be able to move forward north" do
+	describe "moving" do
+    context "facing north" do
       rover = MarsRover.new 5,5,:n,10,10
-      rover.move_forward
-      expect(rover.position).to eq({:x => 5, :y => 6})
+      it "should be able to move forward" do
+        rover.move_forward
+        expect(rover.position).to eq({:x => 5, :y => 6})
+      end
+      it "should be able to move backwards" do
+        rover.move_backwards
+        expect(rover.position).to eq({:x => 5, :y => 4})      
+      end
     end
-    it "should be able to move forward south" do
+    context "facing south" do
       rover = MarsRover.new 5,5,:s,10,10
-      rover.move_forward
-      expect(rover.position).to eq({:x => 5, :y => 4})
+      it "should be able to move forward south" do
+        rover.move_forward
+        expect(rover.position).to eq({:x => 5, :y => 4})
+      end
     end
-    it "should be able to move forward east" do
+    context "facing east" do
       rover = MarsRover.new 5,5,:e,10,10
-      rover.move_forward
-      expect(rover.position).to eq({:x => 6, :y => 5})
+      it "should be able to move forward east" do
+        rover.move_forward
+        expect(rover.position).to eq({:x => 6, :y => 5})
+      end
     end
-    it "should be able to move forward west" do
+    context "facing west" do
       rover = MarsRover.new 5,5,:w,10,10
-      rover.move_forward
-      expect(rover.position).to eq({:x => 4, :y => 5})
+      it "should be able to move forward west" do
+        rover.move_forward
+        expect(rover.position).to eq({:x => 4, :y => 5})
+      end
     end
   end
 end
