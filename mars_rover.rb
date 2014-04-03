391064c47ea971f3afe7bcdd733fc27b34aa28cb
Nieve <nievegoor@gmail.com>
2014-04-03 21:24:45 +0100

    added move backwards feature for all 4 directions

--- mars_rover.rb
+++ mars_rover.rb
@@ -7,19 +7,28 @@ class MarsRover
     @length = length
   end
   def move_forward
-  	case @direction
-  	  when :n
+    case @direction
+      when :n
         @y += 1
-  	  when :s
-  	    @y -= 1
-  	  when :e
-  	    @x += 1
-  	  when :w
-  	    @x -= 1
-  	end
+      when :s
+        @y -= 1
+      when :e
+        @x += 1
+      when :w
+        @x -= 1
+    end
   end
   def move_backwards
-    @y -= 1
+    case @direction
+      when :n
+        @y += -1
+      when :s
+        @y -= -1
+      when :e
+        @x += -1
+      when :w
+        @x -= -1
+    end
   end
   def position
     {:x => @x, :y => @y}
