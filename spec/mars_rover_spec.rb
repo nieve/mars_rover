d04b30bc0abf0651fdf8faf2d595c740a0f6d6ac
Nieve <nievegoor@gmail.com>
2014-04-03 22:30:58 +0100

    added pivoting specs

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -66,7 +66,7 @@ describe "MarsRover" do
       end
     end
     context "facing west" do
-    	before(:each) do
+      before(:each) do
         @rover = MarsRover.new 5,5,:w,10,10
       end
       it "should be able to move forward" do
@@ -83,7 +83,61 @@ describe "MarsRover" do
       end
       it "should stay facing west after moving backwards" do
         @rover.send('B')
-        expect(@rover.facing).to eq(:w)      
+        expect(@rover.facing).to eq(:w)
+      end
+    end
+  end
+  describe "pivoting" do
+    context "facing west" do
+      before(:each) do
+        @rover = MarsRover.new 5,5,:w,10,10
+      end
+      it "should pivot to the right" do
+        @rover.pivot_right
+        expect(@rover.facing).to eq(:n)
+      end
+      it "should pivot to the left" do
+        @rover.pivot_left
+        expect(@rover.facing).to eq(:s)
+      end
+    end
+    context "facing east" do
+      before(:each) do
+        @rover = MarsRover.new 5,5,:e,10,10
+      end
+      it "should pivot to the right" do
+        @rover.pivot_right
+        expect(@rover.facing).to eq(:s)
+      end
+      it "should pivot to the left" do
+        @rover.pivot_left
+        expect(@rover.facing).to eq(:n)
+      end
+    end
+    context "facing south" do
+      before(:each) do
+        @rover = MarsRover.new 5,5,:s,10,10
+      end
+      it "should pivot to the right" do
+        @rover.pivot_right
+        expect(@rover.facing).to eq(:w)
+      end
+      it "should pivot to the left" do
+        @rover.pivot_left
+        expect(@rover.facing).to eq(:e)
+      end
+    end
+    context "facing north" do
+      before(:each) do
+        @rover = MarsRover.new 5,5,:n,10,10
+      end
+      it "should pivot to the right" do
+        @rover.pivot_right
+        expect(@rover.facing).to eq(:e)
+      end
+      it "should pivot to the left" do
+        @rover.pivot_left
+        expect(@rover.facing).to eq(:w)
       end
     end
   end
