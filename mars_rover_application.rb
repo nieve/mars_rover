faf66e952aa540fba8a3265e58a88f3d6c0a598f
Nieve <nievegoor@gmail.com>
2014-04-05 01:31:56 +0100

    made application swallow obstacle exception and return position

--- mars_rover_application.rb
+++ mars_rover_application.rb
@@ -1,7 +1,10 @@
 class MarsRoverApplication
-  def self.run x,y,direction,horizontal_size,vertical_size,commands
-    rover = Rover.new x,y,direction,horizontal_size,vertical_size
-    rover.run_commands commands
+  def self.run x,y,direction,horizontal_size,vertical_size,commands,obstacles=nil
+    rover = Rover.new x,y,direction,horizontal_size,vertical_size,obstacles
+    begin
+      rover.run_commands commands
+    rescue ObstacleException
+    end
     rover.position
   end
 end
